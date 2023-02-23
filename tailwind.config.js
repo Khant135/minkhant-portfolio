3389181e678a310d242a055699d98dcaf903251e
Khant135 <minkhant22334456@gmail.com>
2023-02-23 12:37:10 +0630

    tailwinds website

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      // Custormization for screen size
+      sm: '',
+      md: '',
+      lg: '',
+      xl: '',
+    },
+    extend: {
+      colors: {
+        // Custormize Colors
+      }
+    },
+  },
+  plugins: [],
+}
